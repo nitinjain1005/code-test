92aa127e9feacc7fa79f5f2f23162e0558d21a50
Nitin jain <nitinjain1005@gmail.com>
2025-10-24 21:37:35 +0200

    Update rename folder.

--- api/Infra/ExternalServices.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace api.Infra
-{
-    public class ExternalServices
-    {
-        public string ExchangeRateUrl { get; set; } = string.Empty;
-        public string AccountUrl { get; set; } = string.Empty;
-
-    }
-}
